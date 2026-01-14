6dde389a14efc29da8d6fc7c6fd38a06338371e2
Mahadiur <almahadiurrahman@gmail.com>
2026-01-14 09:57:49 -0800

    Online Batch Team app is fully ready and run successfully

--- Dashboard/settings.py
+++ Dashboard/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'FrontendDev',
     'BackendDev',
+    'OnlineBatchTeam',
 ]
 
 MIDDLEWARE = [
--- Dashboard/showcase.py
+++ Dashboard/showcase.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from FrontendDev.models import FrontEndTeam
 from BackendDev.models import BackEndTeam
+from OnlineBatchTeam.models import OnlineBatchMentor
 
 def EmpDashboard(request):
     frontendteam = FrontEndTeam.objects.all()
     backendteam = BackEndTeam.objects.all()
+    onlinebatchteam = OnlineBatchMentor.objects.all()
     context = {
         'Frontend_info': frontendteam,
-        'Backend_info': backendteam
+        'Backend_info': backendteam,
+        'OnlineBatch_info': onlinebatchteam
     }
     return render(request, 'index.html', context )
