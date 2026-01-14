ef77a7cc6afe88cb4868e5044726fd788d2e035f
Mahadiur <almahadiurrahman@gmail.com>
2026-01-14 09:22:37 -0800

    Backend Developer app is fully ready and run successfully

--- Dashboard/showcase.py
+++ Dashboard/showcase.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from FrontendDev.models import FrontEndTeam
+from BackendDev.models import BackEndTeam
 
 def EmpDashboard(request):
     frontendteam = FrontEndTeam.objects.all()
+    backendteam = BackEndTeam.objects.all()
     context = {
-        'Frontend_info': frontendteam
+        'Frontend_info': frontendteam,
+        'Backend_info': backendteam
     }
     return render(request, 'index.html', context )
