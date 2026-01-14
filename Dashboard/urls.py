a7eb93eac2ba7025a4dd0fc7970a441eedd30dfb
Mahadiur <almahadiurrahman@gmail.com>
2026-01-14 10:13:06 -0800

    create indivisuale url and urls file

--- Dashboard/urls.py
+++ Dashboard/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import showcase
 from django.conf import settings
 from django.conf.urls.static import static
@@ -23,5 +23,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', showcase.EmpDashboard)
+    path('', showcase.EmpDashboard),
+    path('frontendteam/', include('FrontendDev.urls')),
+    path('backendteam/', include('BackendDev.urls')),
+    path('onlinebatchteam/', include('OnlineBatchTeam.urls')),
+    path('editorteam/', include('Editorteam.urls'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
