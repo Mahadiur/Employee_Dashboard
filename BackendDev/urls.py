72f14c397e0731a5d99067a8b0595820ffdb23fe
Mahadiur <almahadiurrahman@gmail.com>
2026-01-15 01:00:11 -0800

    Employee Dashboard project is Complete and run in server successfully

--- BackendDev/urls.py
+++ BackendDev/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('<int:pk>/', views.backenddev_details)
+    path('<int:pk>/', views.backenddev_details, name='backend')
 ]
